fd99bc97bca4d98366bad1702c28374a3c1c62a3
GustavoCaso <gustavocaso@gmail.com>
2015-12-28 19:52:25 +0100

    Used `OptionParser` for helping with db_backup arguments

--- easy_to_use/db_backup/bin/db_backup.rb
+++ easy_to_use/db_backup/bin/db_backup.rb
@@ -18,6 +18,24 @@ option_parser = OptionParser.new do |opts|
   opts.on("-p PASSWORD") do |password|
     options[:password] = password
   end
+
+  # you dont have to use always regular expressions for validation. By including
+  # an Array you can indicate the complete list of acceptable values.
+  # By using a Hash will use the keys as acceptable values and send the mapped
+  # value to the block.
+  servers = {
+    'dev'  => '127.0.01',
+    'qa'   => 'qa001.example.com',
+    'prod' => 'www.example.com'
+  }
+
+  opts.on("--server SERVER", servers) do |address|
+    options[:server] = address
+  end
+
+  # Also you can pass a classname in the argument list, `OptionParser` will
+  # attempt to convert the string from the command line an instance of the given
+  # class
 end
 
 # Difference between Swutch and flag, when parsing arguments
@@ -26,4 +44,15 @@ end
 # and another string it's treated as a flag
 
 option_parser.parse!
-puts options.inspect
+
+database = ARGV.shift
+
+unless options[:iteration].nil?
+  backup_file = database + Time.now.strftime('%Y%m%d')
+else
+  backup_file = database + options[:iteration].to_s
+end
+mysqldump = "mysqldump -u#{options[:username]} - p#{options[:password]} #{database}"
+`#{mysqldump} > #{backup_file}.sql`
+`gzip #{backup_file}.sql`
+
